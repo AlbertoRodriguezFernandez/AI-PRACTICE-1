b2a91641da096affe621dfbb5049e0a963152500
AlbertoRodriguezFernandez <arf170903@gmail.com>
2024-03-24 01:00:44 +0100

    Cambios tontorrones

--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -7,18 +7,29 @@ using namespace std;
 class ComportamientoJugador : public Comportamiento{
 
   public:
+
+    // Constructor
     ComportamientoJugador(unsigned int size) : Comportamiento(size){
-      // Constructor de la clase
-      // Dar el valor inicial a las variables de estado
+      
+      // TO-DO: Dar el valor inicial a las variables de estado
     }
 
+    // Constructor de copia
     ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
+    
+    // Destructor
     ~ComportamientoJugador(){}
 
+    // Método think --> describe comportamiento del agente
     Action think(Sensores sensores);
+
+    // Método interact --> descibe como interacciona agente con otros agentes
     int interact(Action accion, int valor);
 
   private:
-  // Declarar aquí las variables de estado
+  
+  // TO-DO: Declarar aquí las variables de estado que almacenan memoria sobre lo 
+  // que lleva conocido el agente sobre el mundo
 };
+
 #endif
