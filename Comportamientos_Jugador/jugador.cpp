6403e581413360fa973ac4048f9911dadde502bd
AlbertoRodriguezFernandez <arf170903@gmail.com>
2024-03-30 17:13:11 +0100

    cambios 30 17:13

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -34,15 +34,6 @@ void ComportamientoJugador::valor_sensores(Sensores sensores) {
 }
 
 
-/*
-// Método PonerTerrenoEnMatriz
-void PonerTerrenoEnMatriz(const vector<unsigned char> &terreno, const state &st, vector<vector<unsigned char>> &matriz) {
-
-	matriz[st.fil][st.col] = terreno[0];
-}
-*/
-
-
 // Método think
 Action ComportamientoJugador::think(Sensores sensores)
 {
@@ -110,14 +101,52 @@ Action ComportamientoJugador::think(Sensores sensores)
 		break;   
 	}
 
-	// Accion
+	/* 
+	
+	----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-	// 1º: Buscar casilla de posicionamiento
-	// 2º: Buscar bikini y zapatillas
-	// 3º: No repetir movimiento a casillas visitadas
-	// 3º: Gestionar precipios y muros
-	// 4º: Gestionar aldeanos y lobos
+	SECUENCIA DE ACCIONES
 
+	1º: Comprobar SENSORES DE VISIÓN para saber en que nivel estoy y saber si conozco posición o no.
+
+	2º: MATRIZ DE VISIÓN 
+		
+			2.1 Ver lo que tengo delante y guardar en MATRIZ DE VISIÓN (pensar también si estoy pegado a los precipicios)
+
+				2.1.1 Si lo que tengo delante SÍ es interesante: ZAPATILLA, BIKINI, RECARGA o CASILLA POSICIONAMIENTO --> ir hacia alli y guardarmelo
+
+					¿Como voy?
+						Si a donde quiero ir está a mi izquierda: me muevo por filas y luego por columnas (avanzo, giro 90ª izquierda y avanzo)
+						Si a donde quiero ir está a mi derecha: miro diagonal (fila - columna = 0) y si no está, avanzo hacia delante y vuelvo a mirar diagonal
+					
+					Caso especial
+						Si el coste de ir a la recarga es mayor que lo que da la recarga de bateria (10) no merece la pena ir. En caso contrario si
+
+				2.1.2 Si lo que tengo delante NO es interesante: 
+
+					2.1.2.1 Si estoy en agua o bosque sin bikini o zapatillas: 
+						2.1.2.1.1 Busco en las casillas de delante si puedo irme a tierra que tienen coste menor.
+						2.1.2.1.2 Giro 90º a la izquierda y repetir paso 2.1
+
+
+	3º: MATRIZ DE YA VISITADAS
+
+		Ir guardando las casillas visitadas como elemento[i][j] = true y así no volver a visitarlas
+
+	4º: MATRIZ MAPA RESULTADO
+
+		Nivel 0: nada mas pisar puedo empezar a guardar en mapaResultado.
+		Nivel 1,2,3: hasta que no este en casilla de posicionamiento no puedo empezar a guardar en mapaResultado.
+	
+		// Método PonerTerrenoEnMatriz
+		void PonerTerrenoEnMatriz(const vector<unsigned char> &terreno, const state &st, vector<vector<unsigned char>> &matriz) {
+
+			matriz[st.fil][st.col] = terreno[0];
+		}
+		
+
+	-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	
 	if (sensores.terreno.size() != 0 && sensores.agentes.size() != 0) {
 		
 		// Casilla de posicionamiento
@@ -191,6 +220,7 @@ Action ComportamientoJugador::think(Sensores sensores)
 		cerr << "Vectores vacios" << endl;
 		accion = actIDLE;
 	}
+	*/
 	
 
 	// Devolver el valor de la accion
