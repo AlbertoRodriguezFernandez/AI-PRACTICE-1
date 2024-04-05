272ebee5c6993cee83afbedf0383635abba53bc5
AlbertoRodriguezFernandez <arf170903@gmail.com>
2024-04-05 22:25:43 +0200

    Observacion terminada y movimiento basico, visitados disponibles
    La matriz de visitaados si esta actualizada cuando se puede pero no se
    esta usando en las reglas de decision

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 
 // Método valor_sensores
+
+// TODO: ostream
 void ComportamientoJugador::valor_sensores(Sensores sensores) {
 
 	cout << "Posicion: fila " << sensores.posF << " columna " << sensores.posC;
@@ -30,286 +32,176 @@ void ComportamientoJugador::valor_sensores(Sensores sensores) {
 
 	cout << "\nColision: " << sensores.colision;
 	cout << "  Reset: " << sensores.reset;
-	cout << "  Vida: " << sensores.vida << endl<< endl;
+	cout << "  Vida: " << sensores.vida;
+	cout << " Tiempo: " << sensores.tiempo << endl << endl;
+}
+
+
+// Método valor_current_state
+void ComportamientoJugador::valor_current_state() {
+
+	cout << "Current_State:";
+	cout << " Fila: " << current_state.fil;
+	cout << " Columna: " << current_state.col;
+	cout << " Orientacion: ";
+	if (current_state.brujula_desconocida) {cout << " Desconocida " << endl;}
+	else cout << current_state.brujula << endl;
 }
 
 
 // Método think
 Action ComportamientoJugador::think(Sensores sensores)
 {
-	Action accion = actIDLE;
-	int nueva_orientacion;
+	// Comprobación de nivel en el momento inicial para completar la inicialización del objeto
+	if (sensores.tiempo == 0) {
+
+		// SI se conoce la orientacion inicial
+		if (sensores.nivel == 1 || sensores.nivel == 2) {
+
+			current_state.brujula = norte;
+			current_state.brujula_desconocida = false;
+		} 
+	}
+	
+
 
 	// Imprimir valor de los sensores
-	this->valor_sensores(sensores);
+	valor_sensores(sensores);
+	
 
-	// Actualización variables de estado
-	switch(last_action)
-	{
-		case actWALK:   
 
-			switch (current_state.brujula)
-			{			
-			case norte :   current_state.fil--;                      break;
-			case noreste:  current_state.fil--; current_state.col++; break; 
-			case este:     current_state.col++;                      break;
-			case sureste:  current_state.fil++; current_state.col++; break;
-			case sur:      current_state.fil++;                      break;
-			case suroeste: current_state.fil++; current_state.col--; break;
-			case oeste:    current_state.col--;                      break;
-			case noroeste: current_state.fil--; current_state.col--; break;
-			}
 
-		break; 
+	// OBSERVACION y actualización variables de estado: posibilidad de que sea sensores o a través de la ultima accion
 
-		case actRUN:      
+	// 1. Sensores
+	if (sensores.posF != POS_DESCONOCIDA && sensores.posC != POS_DESCONOCIDA) {
 
-			switch (current_state.brujula)
-			{			
-			case norte :   current_state.fil-=2;                        break;
-			case noreste:  current_state.fil-=2; current_state.col+=2;  break; 
-			case este:     current_state.col+=2;                        break;
-			case sureste:  current_state.fil+=2; current_state.col+=2;  break;
-			case sur:      current_state.fil+=2;                        break;
-			case suroeste: current_state.fil+=2; current_state.col-=2;  break;
-			case oeste:    current_state.col-=2;                        break;
-			case noroeste: current_state.fil-=2; current_state.col-=2;  break;
-			}
+		current_state.fil = sensores.posF;
+		current_state.col = sensores.posC;
+		current_state.brujula = sensores.sentido;
+		current_state.brujula_desconocida = false;
+	
+	} else {
 
-		break;
+		// 2. Ultima accion
+		switch(last_action)
+		{
+			case actWALK:   
 
-		case actTURN_SR:  
+				if (!current_state.brujula_desconocida && current_state.fil != POS_DESCONOCIDA && current_state.col != POS_DESCONOCIDA) {
 
-			nueva_orientacion = current_state.brujula;
-			nueva_orientacion = (nueva_orientacion + 1) % 8;
-			current_state.brujula = static_cast<Orientacion>(nueva_orientacion);
-			
-		break;
+					switch (current_state.brujula)
+					{			
+					case norte :   current_state.fil--;                      break;
+					case noreste:  current_state.fil--; current_state.col++; break; 
+					case este:     current_state.col++;                      break;
+					case sureste:  current_state.fil++; current_state.col++; break;
+					case sur:      current_state.fil++;                      break;
+					case suroeste: current_state.fil++; current_state.col--; break;
+					case oeste:    current_state.col--;                      break;
+					case noroeste: current_state.fil--; current_state.col--; break;
+					}
 
-		case actTURN_L: 
+				}
 
-			nueva_orientacion = current_state.brujula;
-			nueva_orientacion = (nueva_orientacion + 6) % 8;
-			current_state.brujula = static_cast<Orientacion>(nueva_orientacion);
+			break; 
 
-		break;	   
+			case actRUN:     
 
-		case actIDLE:
+				if (!current_state.brujula_desconocida && current_state.fil != POS_DESCONOCIDA && current_state.col != POS_DESCONOCIDA) {
 
-			cout << "No se hace nada" << endl;
+					switch (current_state.brujula)
+					{			
+					case norte :   current_state.fil-=2;                        break;
+					case noreste:  current_state.fil-=2; current_state.col+=2;  break; 
+					case este:     current_state.col+=2;                        break;
+					case sureste:  current_state.fil+=2; current_state.col+=2;  break;
+					case sur:      current_state.fil+=2;                        break;
+					case suroeste: current_state.fil+=2; current_state.col-=2;  break;
+					case oeste:    current_state.col-=2;                        break;
+					case noroeste: current_state.fil-=2; current_state.col-=2;  break;
+					}
+				}
 
-		break;   
+			break;
+
+			case actTURN_SR:  
+
+				if (!current_state.brujula_desconocida) {
+
+					int nueva_orientacion = current_state.brujula;
+					nueva_orientacion = (nueva_orientacion + 1) % 8;
+					current_state.brujula = static_cast<Orientacion>(nueva_orientacion);
+				}
+				
+			break;
+
+			case actTURN_L: 
+
+				if (!current_state.brujula_desconocida) {
+
+					int nueva_orientacion = current_state.brujula;
+					nueva_orientacion = (nueva_orientacion + 6) % 8;
+					current_state.brujula = static_cast<Orientacion>(nueva_orientacion);
+				}
+
+			break;	   
+		}
+
 	}
+
+	// Imprimir valor de current_state
+	valor_current_state();
+
+
+	// Si conozco la pos guardo el INSTANTE en el que paso por la casilla que sea
+	if (current_state.fil != POS_DESCONOCIDA && current_state.col != POS_DESCONOCIDA) {
+
+		matriz_ultimas_visitas[current_state.fil][current_state.col] = sensores.tiempo;
+
+	}
+	
+
+	
+	// DECISION
+	Action next_action = actIDLE;
 
 	/* 
+	INFO CON LA QUE CUENTO
+
+		1. current_state
+
+			a. pos / orientacion conocida    --> if (current_state.fil/col != POS_DESCONOCIDA && !brujula_desconocida)
+												 Si puedo actualizar y usar matriz visitas
+
+			b. pos / orientacion desconocida --> else 
+												 No puedo actualizar ni usar la matriz visitas
+
+	INFO DE ULTIMAS VISITAS
+
+		matriz_ultimas_visitas[fil][col] == sensores.tiempo
 	
-	----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-	SECUENCIA DE ACCIONES
-
-	1º: Comprobar SENSORES DE VISIÓN para saber en que nivel estoy y saber si conozco posición o no.
-
-	2º: MATRIZ DE VISIÓN 
-		
-			2.1 Ver lo que tengo delante y guardar en MATRIZ DE VISIÓN (pensar también si estoy pegado a los precipicios)
-
-				2.1.1 Si lo que tengo delante SÍ es interesante: ZAPATILLA, BIKINI, RECARGA o CASILLA POSICIONAMIENTO --> ir hacia alli y guardarmelo
-
-					¿Como voy?
-						Si a donde quiero ir está a mi izquierda: me muevo por filas y luego por columnas (avanzo, giro 90ª izquierda y avanzo)
-						Si a donde quiero ir está a mi derecha: miro diagonal (fila - columna = 0) y si no está, avanzo hacia delante y vuelvo a mirar diagonal
-					
-					Caso especial
-						Si el coste de ir a la recarga es mayor que lo que da la recarga de bateria (10) no merece la pena ir. En caso contrario si
-
-				2.1.2 Si lo que tengo delante NO es interesante: 
-
-					2.1.2.1 Si estoy en agua o bosque sin bikini o zapatillas: 
-						2.1.2.1.1 Busco en las casillas de delante si puedo irme a tierra que tienen coste menor.
-						2.1.2.1.2 Giro 90º a la izquierda y repetir paso 2.1
-
-
-	3º: MATRIZ DE YA VISITADAS
-
-		Ir guardando las casillas visitadas como elemento[i][j] = true y así no volver a visitarlas
-
-	4º: MATRIZ MAPA RESULTADO
-
-		Nivel 0: nada mas pisar puedo empezar a guardar en mapaResultado.
-		Nivel 1,2,3: hasta que no este en casilla de posicionamiento no puedo empezar a guardar en mapaResultado.
-	
-		// Método PonerTerrenoEnMatriz
-		void PonerTerrenoEnMatriz(const vector<unsigned char> &terreno, const state &st, vector<vector<unsigned char>> &matriz) {
-
-			matriz[st.fil][st.col] = terreno[0];
-		}
-		
-
-	----------------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
-
-	*/
-
-	// Comprobar en que nivel estoy
-
-	if (sensores.terreno.size() != 0 && sensores.agentes.size() != 0) {
-
-		// Niveles 1,2,3
-		if (sensores.posF == -1 && sensores.posC == -1) {
-
-			cout << "Hola" << endl;
-
-		// Nivel 0
-		} else {
-
-			// Situarse bien
-			if (sensores.terreno[0] != 'M' && sensores.terreno[0] != 'P' && !bien_situado) {
-
-				current_state.fil = sensores.posF;
-				current_state.col = sensores.posC;
-				current_state.brujula = sensores.sentido;
-				bien_situado = true;
-			}
-
-			if (bien_situado) {
-
-				mapaResultado[current_state.fil][current_state.col] = sensores.terreno[0];
-			}
-
-			// Zapatillas
-			if (sensores.terreno[0] == 'D' && !tengo_zapatillas) {
-
-				current_state.fil = sensores.posF;
-				current_state.col = sensores.posC;
-				current_state.brujula = sensores.sentido;
-				tengo_zapatillas = true;
-			}
-
-			// Bikini
-			if (sensores.terreno[0] == 'K' && !tengo_bikini) {
-
-				current_state.fil = sensores.posF;
-				current_state.col = sensores.posC;
-				current_state.brujula = sensores.sentido;
-				tengo_bikini = true;
-			}
-
-			if ((sensores.terreno[2] == 'T' || sensores.terreno[2] == 'S' || sensores.terreno[2] == 'G' || sensores.terreno[2] == 'D' || sensores.terreno[2] == 'K' || sensores.terreno[2] == 'X') && sensores.agentes[2] == '_' ) {
-
-				accion = actWALK;
-
-			// Agua
-			} else if (tengo_bikini && sensores.terreno[2] == 'A') {
-
-				accion = actWALK;
-
-			// Bikini
-			} else if (tengo_zapatillas && sensores.terreno[2] == 'B') {
-
-				accion = actWALK;
-
-			} else if (!girar_derecha) {
-
-				accion = actTURN_L;
-				girar_derecha = (rand() % 2 == 0);
-			
-			} else {
-
-				accion = actTURN_SR;
-				girar_derecha = (rand() % 2 == 0);
-			}
-		}
-
-
-	} else {
-
-		cerr << "Vectores vacios" << endl;
-		accion = actIDLE;
-	}
-
-
-
-
-	/*
-	if (sensores.terreno.size() != 0 && sensores.agentes.size() != 0) {
-		
-		// Casilla de posicionamiento
-		if (sensores.terreno[0] == 'G' && !bien_situado) {
-
-			current_state.fil = sensores.posF;
-			current_state.col = sensores.posC;
-			current_state.brujula = sensores.sentido;
-			bien_situado = true;
-		}
-
-		// Zapatillas
-		if (sensores.terreno[0] == 'D' && !tengo_zapatillas) {
-
-			current_state.fil = sensores.posF;
-			current_state.col = sensores.posC;
-			current_state.brujula = sensores.sentido;
-			tengo_zapatillas = true;
-		}
-
-		// Bikini
-		if (sensores.terreno[0] == 'K' && !tengo_bikini) {
-
-			current_state.fil = sensores.posF;
-			current_state.col = sensores.posC;
-			current_state.brujula = sensores.sentido;
-			tengo_bikini = true;
-		}
-
-		if ((sensores.terreno[2] == 'T' || sensores.terreno[2] == 'S' || sensores.terreno[2] == 'G' || sensores.terreno[2] == 'D' || sensores.terreno[2] == 'K' || sensores.terreno[2] == 'X') && sensores.agentes[2] == '_' ) {
-
-			accion = actWALK;
-
-		// Agua
-		} else if (tengo_bikini && sensores.terreno[2] == 'A') {
-
-			accion = actWALK;
-
-		// Bikini
-		} else if (tengo_zapatillas && sensores.terreno[2] == 'B') {
-
-			accion = actWALK;
-
-		// Bateria
-		} else if (sensores.terreno[2] == 'X' && sensores.bateria <= 4990) {
-
-			sensores.bateria += 10;
-
-		} else if (!girar_derecha) {
-
-			accion = actTURN_L;
-			girar_derecha = (rand() % 2 == 0);
-		
-		} else {
-
-			accion = actTURN_SR;
-			girar_derecha = (rand() % 2 == 0);
-		}
-		
-
-		// Guardar en mapaResultado
-		if (bien_situado) {
-
-			mapaResultado[current_state.fil][current_state.col] = sensores.terreno[0];
-			// PonerTerrenoEnMatriz(sensores.terreno, current_state, mapaResultado);
-		}
-		
-
-	} else {
-
-		cerr << "Vectores vacios" << endl;
-		accion = actIDLE;
-	}
 	*/
 	
 	
+
+	if ((sensores.terreno.size() > 2 && sensores.agentes.size() > 2) and (sensores.terreno[2] == 'T' or sensores.terreno[2] == 'S') and sensores.agentes[2] == '_'){
+		
+		next_action = actWALK;
+		cout << "next_action = actWALk" << endl << endl;
+	
+	} else{
+		
+		next_action = actTURN_L;
+		cout << "next_action = actTURN_L" << endl << endl;
+	}
+	
+
+
 
 	// Devolver el valor de la accion
-	last_action = accion;
-	return accion;
+	last_action = next_action;
+	return next_action;
 }
 
 
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -12,6 +12,7 @@ struct state {
   int fil;
   int col;
   Orientacion brujula;
+  bool brujula_desconocida;
 };
 
 
@@ -21,22 +22,41 @@ class ComportamientoJugador : public Comportamiento{
 
   public:
 
+    static const int POS_DESCONOCIDA = -1; // Valor fuera de rango porque no se la pos
+
     // Constructor
     ComportamientoJugador(unsigned int size) : Comportamiento(size){
       
-      // Inicializar variables de estado
       last_action = actIDLE;
-      current_state.fil = current_state.col = 99;
-      current_state.brujula = norte;    
-      girar_derecha = false;  
+      current_state.fil = current_state.col = POS_DESCONOCIDA;  
+
+      // Deliberadamente no inicializado current_state.brujula porque necesito el nivel y eso lo hare en el think  
+      current_state.brujula_desconocida = true;
+       
       bien_situado = false;
       tengo_zapatillas = false;
       tengo_bikini = false;
+
+      // Inicialización de matriz ultimas visitas
+      vector<double> aux(size, 0.0);
+      for (int i = 0; i < size; i++) {
+
+        matriz_ultimas_visitas.push_back(aux);
+      }
+
     }
 
 
     // Constructor de copia
-    ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
+    ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport) {
+
+      last_action = comport.last_action;
+      current_state = comport.current_state;
+      bien_situado = comport.bien_situado;
+      tengo_zapatillas = comport.tengo_zapatillas;
+      tengo_bikini = comport.tengo_bikini;
+      matriz_ultimas_visitas = comport.matriz_ultimas_visitas;
+    }
     
 
     // Destructor
@@ -55,6 +75,10 @@ class ComportamientoJugador : public Comportamiento{
     void valor_sensores(Sensores sensores);
 
 
+    // Método que muestra el valor del current_state
+    void valor_current_state();
+
+
     // Método que se encarga de colocar toda la info en sensor terreno
     void PonerTerrenoEnMatriz(const vector<unsigned char> &terreno, const state &st, vector<vector<unsigned char>> &matriz);
 
@@ -64,10 +88,10 @@ class ComportamientoJugador : public Comportamiento{
     // Variables de estado de memoria del agente
     Action last_action;
     state current_state;
-    bool girar_derecha;
     bool bien_situado;
     bool tengo_zapatillas;
     bool tengo_bikini;
+    vector<vector<double>> matriz_ultimas_visitas;
 };
 
 #endif
